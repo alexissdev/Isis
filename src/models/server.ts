0857c68fd67657f8971b8e3e312f24583f734308
alexissdev <notcacha1998@gmail.com>
2022-11-27 14:03:08 -0300

    fix(server): fix the errors of server model

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,27 +1,25 @@
-import express, { Express } from 'npm:express';
-import loadRoutes from "./loader/routes.loader.ts";
+import express, { Express } from "npm:express";
+import loadRoutes from "../loader/routes.loader.ts";
 
 export default class Server {
+  private application: Express;
 
-    private application: Express;
+  public constructor() {
+    this.application = express();
+  }
 
-    public constructor() {
-        this.application = express();
-    }
+  public start(): void {
+    this.config();
 
-    public start(): void {
-        this.config();
+    const serverPort = Deno.env.get("PORT");
 
-        const serverPort: number = Deno.env.get("PORT") || 3000;
-        this.application.listen(
-                serverPort,
-                () => { console.log(`Server running in ${serverPort} port.`); }
-        );
-    }
+    this.application.listen(serverPort, () => {
+      console.log(`Server running in ${serverPort} port.`);
+    });
+  }
 
-    private config(): void {
-        this.application.use(express.json());
-        loadRoutes();
-    }
-
+  private config(): void {
+    this.application.use(express.json());
+    loadRoutes(this.application);
+  }
 }
